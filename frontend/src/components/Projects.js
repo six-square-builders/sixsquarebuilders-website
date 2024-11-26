34def96bd59f46664e04220a9706dcf02e345e11
HARMESH095 <harmeshgopinathan@gmail.com>
2024-11-26 21:35:21 +0530

    check

---
+++ frontend/src/components/Projects.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './Projects.css';
+
+const Projects = () => {
+    return (
+        <div className="projects">
+            <h1>Our Projects</h1>
+            <p>Here are some of the projects we worked on.</p>
+
+            <div className="project-list">
+                <div className="project-item">
+                    <h2>Project 1</h2>
+                    <p>Description of Project 1</p>
+                </div>
+                <div className="project-list">
+                    <h2>Project 2</h2>
+                    <p>Description of Project 2</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
