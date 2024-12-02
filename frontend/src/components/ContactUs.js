259f6e077cd522bd2801045f89eca9e486073c45
HARMESH095 <harmeshgopinathan@gmail.com>
2024-12-02 16:21:34 +0530

    Updated contact address and aligned information to the left for a professional layout.

--- frontend/src/components/ContactUs.js
+++ frontend/src/components/ContactUs.js
@@ -8,10 +8,9 @@ const ContactUs = () => {
             <p>Feel free to reach out to us using the details below:</p>
 
             <div className="contact-info">
-                <h2>SIX SQUARE BUILDERS</h2>
                 <p><strong>Head Office</strong></p>
-                <p>No.4/370, Pillayar Koil Street, Pallavan Nagar,</p>
-                <p>Jalladampet, Chennai : 600 100, Tamil Nadu, India</p>
+                <p>No. 33, S2, Bazaar Road , Ram Nagar North,</p>
+                <p>Madipakkam chennai - 600 091, Tamil Nadu, India</p>
                 <p><strong>Website:</strong> <a href="http://www.sixsquarebuilders.co.in" target="_blank" rel="noopener noreferrer">www.sixsquarebuilders.co.in</a></p>
                 <p><strong>E-Mail:</strong> <a href="mailto:ssbuilders2012@yahoo.com">ssbuilders2012@yahoo.com</a></p>
                 <p><strong>PAN No.:</strong> ACPFS5219L</p>
