457788dd696324ec2dd39ee7602e1d76443a7140
harmeshgv <harmeshgopinathan@gmail.com>
2025-08-31 06:00:26 +0530

    big: fixed dependencies

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./Header.css";
 import logo from "../assets/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
